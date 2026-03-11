3b263c5b015ddd7e22ac6084413b982ef3bb2044
Eleonora Petzold <42029171+EleonoraPetzold@users.noreply.github.com>
2026-03-11 12:16:45 +0100

    Update Svelte adapter paths to Smm module

--- Console/BExIS.Web.Shell/Areas/SMM/BExIS.Modules.SMM.UI.Svelte/svelte.config.js
+++ Console/BExIS.Web.Shell/Areas/SMM/BExIS.Modules.SMM.UI.Svelte/svelte.config.js
@@ -10,8 +10,8 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: '../BExIS.Modules.Sam.UI/Scripts/svelte', // ../BExIS.Modules.Dcm.UI/Scripts/svelte
-			assets: '../BExIS.Modules.Sam.UI/Scripts/svelte', // ../BExIS.Modules.Dcm.UI/Scripts/svelte
+			pages: '../BExIS.Modules.Smm.UI/Scripts/svelte', // ../BExIS.Modules.Dcm.UI/Scripts/svelte
+			assets: '../BExIS.Modules.Smm.UI/Scripts/svelte', // ../BExIS.Modules.Dcm.UI/Scripts/svelte
 			fallback: null,
 			precompress: true,
 			preprocess: true,
